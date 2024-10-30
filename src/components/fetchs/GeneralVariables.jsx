dd9976ef269077ed1dd56aa99f42d0dc59a1879f
Edgar Mendez <edgarmendezcruz75@gmail.com>
2024-10-30 01:46:04 -0500

    cambio de variables de entorno

--- src/components/fetchs/GeneralVariables.jsx
+++ src/components/fetchs/GeneralVariables.jsx
@@ -1,2 +1,2 @@
-export const ENDOPONT = "http://192.168.112.1:5000/api"
-export const IMAGEENDPOINT = "http://192.168.112.1:5000"
+export const ENDOPONT = "https://admina-api.onrender.com/api"
+export const IMAGEENDPOINT = "https://admina-api.onrender.com"
